59583a7073a7877a5020de9f5768817609b94331
Hanns MP <92124175+HannsMP@users.noreply.github.com>
2024-09-02 03:20:10 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const { resolve } = require('path');
-require('dotenv').config();
+// require('dotenv').config();
 
 const Generador = require('./Generador');
 
